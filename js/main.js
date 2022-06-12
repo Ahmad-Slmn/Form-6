f561862efe2c2bd6f25146f1d0629725fff42a6a
Ahmed-Slmn <adverts890@gmail.com>
2022-06-12 23:02:14 +0300

    Upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var btn = document.querySelector(".box > div button");
 
 function sign() {
 
-    form1.style.left = "0";
+    form1.style.left = "-0";
 
     form2.style.left = "100%";
 
@@ -31,15 +31,16 @@ icun1.onclick = function () {
 
         password1.type = "text";
 
-        this.className = "fa-regular fa-eye";
+        this.className = "fa-regular fa-eye-slash";
 
         this.style.color = "#039884";
 
     } else {
         password1.type = "password";
-        this.className = "fa-regular fa-eye-slash";
+        this.className = "fa-regular fa-eye";
         this.style.color = "#fff"
     }
+
 }
 
 var icun2 = document.querySelector(".last i");
@@ -52,13 +53,40 @@ icun2.onclick = function () {
 
         password2.type = "text";
 
-        this.className = "fa-regular fa-eye";
+        this.className = "fa-regular fa-eye-slash";
 
         this.style.color = "#039884";
 
     } else {
         password2.type = "password";
-        this.className = "fa-regular fa-eye-slash";
+        this.className = "fa-regular fa-eye";
         this.style.color = "#fff"
     }
+}
+
+// Check The Passwords Value
+function check() {
+    if (password1.value.length == 0) {
+
+        icun1.style.display = "none"
+    } else {
+        icun1.style.display = "block"
+    }
+
+
+    if (password2.value.length == 0) {
+
+        icun2.style.display = "none"
+    } else {
+        icun2.style.display = "block"
+    }
+}
+check()
+
+password1.oninput = function () {
+    check()
+}
+
+password2.oninput = function () {
+    check()
 }
